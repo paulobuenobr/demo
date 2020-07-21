e17874fa08cda3031ff096fd8f4e7ce1013a810d
paulo <paulo@icarustech.com.br>
2020-07-21 19:21:46 -0300

    Exclusão de arquivos antigos

--- src/main/java/br/sc/senai/repository/PerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package br.sc.senai.repository;
-
-import br.sc.senai.model.Perfil;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PerfilRepository extends CrudRepository<Perfil, Integer> {
-}
